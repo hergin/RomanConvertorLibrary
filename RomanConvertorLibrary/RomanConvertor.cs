1806d2b3e38e4793861521d5364eb9c07db6dca0
abbiggs <abbiggs@bsu.edu>
2019-09-19 11:05:20 -0400

    Couldn't ever get tests to run, have code but don't know if it works.

--- RomanConvertorLibrary/RomanConvertor.cs
+++ RomanConvertorLibrary/RomanConvertor.cs
@@ -11,15 +11,73 @@ namespace RomanConvertorLibrary
 
 		public static int ToArabic(String roman)
 		{
-			// Replace the following line with the actual code!
-			throw new NotImplementedException();
+			if (romain.Contains("I, V, X, L, C, D, M") == false)
+			{
+				throw new FormatException();
+			}
+			//letter to numbers
+			if (CharValues == null)
+			{
+				CharValues = new Dictionary<char, int>();
+				CharValues.Add('I', 1);
+				CharValues.Add('V', 5);
+				CharValues.Add('X', 10);
+				CharValues.Add('L', 50);
+				CharValues.Add('C', 100);
+				CharValues.Add('D', 500);
+				CharValues.Add('M', 1000);
+			}
+
+			//convert letter values
+			int total = 0;
+			int last_value = 0;
+			for (int i = roman.Length - 1; i >= 0; i--)
+			{
+				int new_value = CharValues[roman[i]];
+
+				//add or subtract
+				if (new_value < last_value)
+					total -= new_value;
+				else
+				{
+					total += new_value;
+					last_value = new_value;
+				}
+			}
+
+			//return the result
+			return total;
 		}
 
 
 		public static String ToRoman(int arabic)
 		{
-			// Replace the following line with the actual code!
-			throw new NotImplementedException();
+			if(arabic < 1 || arabic > 3999)
+			{
+				throw new InvalidOperationException();
+			}
+			string result = "";
+
+			//thousands
+			int num;
+			num = arabic / 1000;
+			result += ThouLetters[num];
+			arabic %= 1000;
+
+			//hundreds
+			num = arabic / 100;
+			result += HundLetters[num];
+			arabic %= 100;
+
+			//tens
+			num = arabic / 10;
+			result += TensLetters[num];
+			arabic %= 10;
+
+			//ones
+			result += OnesLetters[arabic];
+
+			return result;
 		}
 	}
 }
